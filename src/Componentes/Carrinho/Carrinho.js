0cd452aa2f9da43bfa749a3bc0936e91b6f134d7
unknown <cesarrcosta99@gmail.com>
2023-03-09 10:50:43 -0300

    estrutura de arquivos

---
+++ src/Componentes/Carrinho/Carrinho.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+ function Carrinho(){
+    return(
+        <div>
+            
+        </div>
+    )
+ }
---
+++ src/Componentes/Filtro/Filtro.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Filtro(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
